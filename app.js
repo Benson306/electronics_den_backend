f6752baebc1d03cfa675ba1138d478716c1c6dee
Benson Ndiwa <bnkimtai@gmail.com>
2023-05-07 15:30:27 +0300

    Access token

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-const express = require('express');
+let express = require('express');
 
-const app = express();
+let app = express();
+
+const bodyParser = require('body-parser');
+
+const urlEncoded = bodyParser.urlencoded({extended: false});
+
+let unirest = require('unirest');
+
+require('dotenv').config();
+
+app.use(express.json())
+
+function access_token(){
+
+    let req = unirest('POST', `https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken`)
+    .headers({
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+    })
+    .send(JSON.stringify({
+        "consumer_key": process.env.CONSUMER_KEY,
+        "consumer_secret": process.env.CONSUMER_SECRET
+    }))
+    .end(response => { 
+        //if (res.error) throw new Error(res.error); 
+        // if (response.error) {console.log(response.error)};
+        let result = JSON.parse(response.raw_body);
+        console.log(result)
+        //res.json(result)
+    });
+}
+
+access_token();
 
 app.get('/', function(req, res){
-    res.send('Home');
+
+    access_token();
+    
+    res.json(req.body);
 })
 
 app.listen(3000)
