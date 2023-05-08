dd821f2a064db8eb05d5949e35b7eff87ae97010
Benson Ndiwa <bnkimtai@gmail.com>
2023-05-08 13:48:43 +0300

    IPN Request Done

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.use(cors());
 
 const unirest = require('unirest');
 
-
-
 function accessToken(req, res, next){
 
     unirest('POST', 'http://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken/')
@@ -45,6 +43,32 @@ app.get('/', accessToken, function(req, res){
     let token = req.access_token;
 
     res.json(token);
+})
+
+app.get('/getIpn', accessToken, function(req, res){
+    
+    unirest('POST', 'http://cybqa.pesapal.com/pesapalv3/api/URLSetup/RegisterIPN/')
+    .headers({
+        "Content-Type" : "application/json",
+        "Accept" : "application/json",
+        "Authorization": "Bearer " + req.access_token
+    })
+    .send({
+        "url" : "https://f2db-102-212-236-141.ngrok-free.app/ipn_callback",
+        "ipn_notification_type" : "POST"
+    })
+    .end(response => {
+        if (response.error) throw new Error(response.error);
+
+        console.log(response.raw_body);
+    });
+
+    res.json('success')
+})
+
+app.post('/ipn_callback', urlEncoded, function(req, res){
+    console.log(req.body);
+    req.json('success')
 })
 
 app.listen(3000)
