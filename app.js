ba987fc908fd83e2d166f3c2ccf7a395234ae08c
Benson Ndiwa <bnkimtai@gmail.com>
2023-05-08 13:12:48 +0300

    Access Token Obtained

--- app.js
+++ app.js
@@ -6,39 +6,45 @@ const bodyParser = require('body-parser');
 
 const urlEncoded = bodyParser.urlencoded({extended: false});
 
-let unirest = require('unirest');
-
 require('dotenv').config();
 
 app.use(express.json())
 
-function access_token(){
+let cors = require('cors');
 
-    let req = unirest('POST', `https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken`)
+app.use(cors());
+
+const unirest = require('unirest');
+
+
+
+function accessToken(req, res, next){
+
+    unirest('POST', 'http://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken/')
     .headers({
         "Content-Type" : "application/json",
         "Accept" : "application/json"
     })
-    .send(JSON.stringify({
-        "consumer_key": process.env.CONSUMER_KEY,
-        "consumer_secret": process.env.CONSUMER_SECRET
-    }))
-    .end(response => { 
-        //if (res.error) throw new Error(res.error); 
-        // if (response.error) {console.log(response.error)};
-        let result = JSON.parse(response.raw_body);
-        console.log(result)
-        //res.json(result)
+    .send({
+        "consumer_key" : process.env.CONSUMER_KEY,
+        "consumer_secret" : process.env.CONSUMER_SECRET
+    })
+    .end(response => {
+        if (response.error) throw new Error(response.error);
+
+        let token = response.raw_body.token
+
+        req.access_token = token;
+
+        next();
     });
 }
 
-access_token();
+app.get('/', accessToken, function(req, res){
 
-app.get('/', function(req, res){
+    let token = req.access_token;
 
-    access_token();
-    
-    res.json(req.body);
+    res.json(token);
 })
 
 app.listen(3000)
