18c8590db339eb11f47b3a4d55723295b57e46ab
Benson Ndiwa <bnkimtai@gmail.com>
2024-04-27 12:34:05 +0300

    Cors

--- app.js
+++ app.js
@@ -35,6 +35,15 @@ app.get('/',(req, res)=>{
     res.json('Iko Nini');
 })
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
 let port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
